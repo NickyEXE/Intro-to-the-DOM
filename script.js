3682f5afccd0dde22e9cdf0c608bb901c91e1e29
NickyEXE <dovern42@gmail.com>
2020-08-25 17:02:48 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// variables
+// functions
+// event listeners
+
+// const button = document.querySelector("#macavitoggle")
+
+// const img = document.querySelector("#main-image")
+
+// function macavitoggle(){
+//   if (button.className === "uninvited"){
+//     img.src = "./macavity.gif"
+//     img.alt = "Macavity waving his hat"
+//     button.innerText = "Get rid of Macavity!"
+//     button.className = "invited"
+//   }
+//   else {
+//     img.src = "https://66.media.tumblr.com/71449669428a85307a1485966fd4e100/339a1b67e9516e33-27/s400x600/c9473bb15cbd3854997cb1631e53566acb9a50a2.gifv"
+//     img.alt = "Munkustrap looking excited"
+//     button.innerText = "Invite Macavity to the Jellicle Ball!"
+//     button.className = "uninvited"
+//   }
+// }
+
+// button.addEventListener("click", macavitoggle)
+
+// // macavitoggle()
